8b3efcc6bdaa50c8803064d26f051f2b2a2eef65
jinboker <jinboker@foxmail.com>
2016-06-30 22:00:27 +0800

    6.301

--- JDpage/js/js.js
+++ JDpage/js/js.js
@@ -3,67 +3,66 @@
  * 把多个函数同时绑定到onload事件上面
  * @param {function} fn 想要添加至onload的函数
  */
-// function myReady (fn) {
-// 	var oldonload = window.onload;
-//     //如果onload没有绑定过函数
-// 	if (typeof window.onload != 'function'){
-// 		window.onload = fn;
-// 	}else{
-// 		window.onload = function () {
-// 			oldonload();
-//     		fn();
-//     	};
-//     }
-// }
-
+function myReady (fn) {
+ var oldonload = window.onload;
+    //如果onload没有绑定过函数
+ if (typeof window.onload != 'function'){
+     window.onload = fn;
+ }else{
+     window.onload = function () {
+         oldonload();
+         fn();
+     };
+    }
+}
 /**
  * DomReady,在文档创建时,就访问dom元素.比window.onload要快很多.绑定需要执行的函数.
  * @param  {Function}     fn     需要执行的函数
  * @return {执行}                在dom节点创建时,同时执行,window.onload等到节点全部创建完毕才执行
  */
-function myReady(fn) {
-    //对于现代浏览器，对DOMContentLoaded事件的处理采用标准的事件绑定方式
-    if (document.addEventListener) {
-        document.addEventListener("DOMContentLoaded", fn, false);
-    } else {
-        IEContentLoaded(fn);
-    }
+// function myReady(fn) {
+//     //对于现代浏览器，对DOMContentLoaded事件的处理采用标准的事件绑定方式
+//     if (document.addEventListener) {
+//         document.addEventListener("DOMContentLoaded", fn, false);
+//     } else {
+//         IEContentLoaded(fn);
+//     }
 
-    //IE模拟DOMContentLoaded
-    function IEContentLoaded(fn) {
-        var d = window.document;
-        var done = false;
+//     //IE模拟DOMContentLoaded
+//     function IEContentLoaded(fn) {
+//         var d = window.document;
+//         var done = false;
 
-        //只执行一次用户的回调函数init();
-        var init = function () {
-            if (!done) {
-                done = true;
-                fn();
-            }
-        };
-        (function () {
-            try {
-                // DOM树未创建完之前调用doScroll会抛出错误
-                d.documentElement.doScroll('left');
-            } catch (e) {
-                //延迟再试一次~
-                setTimeout(arguments.callee, 50);
-                return;
-            }
-            // 没有错误就表示DOM树创建完毕，然后立马执行用户回调
-            init();
-        })();
+//         //只执行一次用户的回调函数init();
+//         var init = function () {
+//             if (!done) {
+//                 done = true;
+//                 fn();
+//             }
+//         };
+//         (function () {
+//             try {
+//                 // DOM树未创建完之前调用doScroll会抛出错误
+//                 d.documentElement.doScroll('left');
+//             } catch (e) {
+//                 //延迟再试一次~
+//                 setTimeout(arguments.callee, 50);
+//                 return;
+//             }
+//             // 没有错误就表示DOM树创建完毕，然后立马执行用户回调
+//             init();
+//         })();
 
-        //监听document的加载状态
-        d.onreadystatechange = function () {
-            // 如果用户是在domReady之后绑定的函数，就立马执行
-            if (d.readyState === 'complete') {
-                d.onreadystatechange = null;
-                init();
-            }
-        };
-    }
-}
+//         //监听document的加载状态
+//         d.onreadystatechange = function () {
+//             // 如果用户是在domReady之后绑定的函数，就立马执行
+//             if (d.readyState === 'complete') {
+//                 d.onreadystatechange = null;
+//                 init();
+//             }
+//         };
+//     }
+// }
 
 /**
  *事件添加函数
