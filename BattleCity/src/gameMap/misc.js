770d07e6831fabd412e8b76861f2382e64a7cb65
jinboker <jinboker@foxmail.com>
2016-08-04 13:09:23 +0800

    修复了剩余NPC数目的图形的显示BUG

--- BattleCity/src/gameMap/misc.js
+++ BattleCity/src/gameMap/misc.js
@@ -1,7 +1,3 @@
-let m_canEN = document.createElement('canvas');
-m_canEN.width = m_canEN.height = 18;
-m_canEN.getContext('2d').drawImage(oImg.misc , 0 , 16 , 16 , 16 , 2 , 2 , 16 , 16);
-
 // 敌军剩余数目
 function enemyNum() {
 	let x = 0 , y = 0;
@@ -12,7 +8,7 @@ function enemyNum() {
 			x --;
 			y ++;
 		}
-		cxt.bg.drawImage(m_canEN , 451 + 30 + x * 18 , 20 + y * 18 , 16 , 16);
+		cxt.bg.drawImage(oImg.misc, 0, 16, 16, 16, 481+x*18, 20+y*18, 16, 16);
 	}
 };
 
