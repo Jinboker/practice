17ea837d97b231d2a5d0ef36e85896cb685c5a7f
jinboker <jinboker@foxmail.com>
2016-08-14 23:22:16 +0800

    无

--- gallery/src/App.vue
+++ gallery/src/App.vue
@@ -1,64 +1,56 @@
 <template>
-  <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
-  </div>
+    <div class="content">
+        <div class="wrap">
+            <img-sec></img-sec>
+            <controller-nav></controller-nav>
+        </div>
+    </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+// import ImgDatas from '../data/imgDatas.json'
+import ImgSec from './components/ImgSec.vue'
+import ConNav from './components/ConNav.vue'
 
 export default {
-  components: {
-    Hello
-  }
+    components: {
+        "img-sec": ImgSec,
+        "controller-nav": ConNav
+    }
 }
+
 </script>
 
 <style>
-html {
-  height: 100%;
+body,ul,ol,dl,dd,h1,h2,h3,h4,h5,h6,p,input,select,textarea,form{margin: 0; padding: 0;}
+body{font:14px/1.5 "宋体";}
+img{border:none;}
+ul,ol{list-style:none;}
+input,select,textarea{outline:none;border:none;background:none;}
+textarea{resize:none;}
+a{text-decoration:none;}
+
+html, body {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
 }
 
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
+.content {
+    height: 100%;
+    width: 100%;
+    background-color: #fff;
 }
 
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
-
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
+.wrap {
+    position: absolute;
+    top: 50%;
+    margin-top: -400px;
+    width: 100%;
+    height: 800px;
+    background-color: #333;
+    overflow: hidden;
 }
 </style>
