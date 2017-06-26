ce83a6f30fe0f10f554117ecf9fbd5f4fe7fc842
jinboker <jinboker@gmail.com>
2017-06-26 13:34:51 +0800

    optimize: 完成webpack打包的配置

--- src/spirit/spiritCollect.ts
+++
@@ -1,9 +0,0 @@
-import Player from './player';
-import Npc from './npc';
-import Bullet from './bullet';
-
-export default class {
-  static player: Player;
-  static npcArr: Npc[];
-  static bulletArr: Bullet[];
-}
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanPlugin = require("clean-webpack-plugin");
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-//    "server": "webpack-dev-server --devtool eval-source-map --progress --colors --hot --inline --content-base ./dev",
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 //设置输入和输出根目录
 const ROOT_PATH = path.resolve(__dirname);
@@ -12,49 +10,50 @@ const DEV_PATH = path.resolve(ROOT_PATH, 'dev');
 const BUILD_PATH = path.resolve(ROOT_PATH, 'dist');
 
 //获取环境
-// const env = process.env.NODE_ENV;
+const env = process.env.NODE_ENV;
 
 module.exports = {
   entry: './src/index.ts',
   output: {
-    // path: env === 'dev' ? DEV_PATH : BUILD_PATH,
-    path: DEV_PATH,
-    filename: 'bundle.js'
+    path: env === 'prod' ? BUILD_PATH : DEV_PATH,
+    filename: 'bundle.js?[hash]'
   },
-  devtool: 'cheap-module-eval-source-map',
-  // devtool: env === 'dev' ? 'cheap-module-eval-source-map' : 'hidden-source-map',
+  devtool: env === 'prod' ? false : 'cheap-module-eval-source-map',
   devServer: {
-    contentBase: DEV_PATH,
-    hot: true,
-    open: true,
-    inline: true,
-    port: 8008
-    // port: 6000
+    contentBase: ROOT_PATH,
+    port: 8888
   },
   module: {
     rules: [
       {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'ts-loader']
+      },
+      {
+        'test': /\.(jsx?)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
         test: /\.scss$/,
         exclude: /node_modules/,
-        loaders: ['style-loader', 'css-loader?sourceMap', 'sass-loader?sourceMap']
-        // use: ExtractTextPlugin.extract({
-        //   fallback: "style-loader",
-        //   use: [{
-        //     loader: "css-loader"
-        //   }, {
-        //     loader: "sass-loader"
-        //   }]
-        // })
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [{
+            loader: "css-loader",
+            options: {
+              minimize: env === 'prod'
+            }
+          }, {
+            loader: "sass-loader"
+          }]
+        })
       },
       {
         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
         exclude: /node_modules/,
         use: 'url-loader?limit=30000'
-      },
-      {
-        test: /\.ts(x?)$/,
-        exclude: /node_modules/,
-        use: "ts-loader"
       }
     ]
   },
@@ -65,37 +64,32 @@ module.exports = {
       'src': path.resolve(__dirname, './src')
     }
   },
-  // watch: env === 'dev' ? true : false
-  plugins: [
-    new ExtractTextPlugin("style.css"),
-    new HtmlWebpackPlugin({template: './index.html'})
-  ]
+  plugins: [ new ExtractTextPlugin("style.css?[contenthash]") ]
 };
-//
-//
-// if (env === 'dev') {
-//   module.exports.plugins = (module.exports.plugins || []).concat([
-//     new HtmlWebpackPlugin({template: './index.html'})
-//   ]);
-// } else {
-//   module.exports.plugins = (module.exports.plugins || []).concat([
-//     new HtmlWebpackPlugin({
-//       template: './index.html',
-//       minify: {
-//         removeComments: true,
-//         collapseWhitespace: true,
-//         showErrors: true
-//       }
-//     }),
-//     new CleanPlugin([BUILD_PATH]),
-//     new webpack.optimize.UglifyJsPlugin({
-//       compress: {
-//         warnings: false,
-//         drop_console: true
-//       },
-//       comments: false,
-//       beautify: false,
-//       sourceMap: false
-//     })
-//   ]);
-// }
+
+if (env === 'prod') {
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        showErrors: true
+      }
+    }),
+    new CleanPlugin([BUILD_PATH]),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        drop_console: true
+      },
+      comments: false,
+      beautify: false,
+      sourceMap: false
+    })
+  ]);
+} else {
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new HtmlWebpackPlugin({template: './index.html'})
+  ]);
+}
