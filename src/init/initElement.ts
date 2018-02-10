5bc8e84c3d416369c9f7ea80f68a52b20c14e311
jinboker <jinboker@gmail.com>
2018-02-10 14:40:44 +0800

    feat: 初始化插入element

--- src/init/initElement.ts
+++ src/init/initElement.ts
@@ -1,3 +1,37 @@
+const createElement = (element: string) => document.createElement(element);
+
+let cxtRole;
+let cxtBg;
+let cxtMisc;
+
 export default function () {
-  console.log(1);
+  const wrap = createElement('div');
+
+  [cxtRole, cxtBg, cxtMisc] = new Array(3).fill(null)
+    .map((ele, index) => {
+      const element = (createElement('canvas') as HTMLCanvasElement);
+
+      if (!index) {
+        element.style.zIndex = '-1';
+      }
+
+      element.width = 516;
+      element.height = 456;
+      wrap.appendChild(element);
+
+      return element.getContext('2d');
+    });
+
+  cxtBg.font = '15px prstart';
+  cxtBg.fillStyle = '#000';
+  cxtBg.textBaseline = 'top';
+  cxtMisc.font = '20px prstart';
+  cxtMisc.fillStyle = '#000';
+  cxtMisc.textBaseline = 'top';
+
+  const body = document.body;
+
+  body.insertBefore(wrap, body.firstChild);
 }
+
+export { cxtRole, cxtBg, cxtMisc };
