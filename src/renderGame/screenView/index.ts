a58f96c2ed6a20acaec962b534d070469199bf8d
jinboker <jinboker@gmail.com>
2019-07-30 10:00:01 +0800

    feat: 删除不需要的文件

--- src/renderGame/screenView/index.ts
+++ src/renderGame/screenView/index.ts
@@ -1,10 +1,12 @@
 import { SelectMode } from './selectMode'
+import { SelectStage } from './selectStage'
 
 type Fn = () => void
 export type ScreenViewType = 'selectMode' | 'selectStage' | 'playing' | 'over'
 export type ScreenView = {
   type: ScreenViewType,
   renderSelectMode: Fn,
+  renderSelectStage: (couldSelectStage: boolean) => void
 }
 
 export const screenView: ScreenView = {
@@ -17,5 +19,15 @@ export const screenView: ScreenView = {
     new SelectMode()
 
     screenView.type = 'selectMode'
+  },
+
+  /**
+   * 进入关卡界面
+   * @param couldSelectStage 是否可以选择关卡，在游戏一开始初始进入第一关的时候(非自定义第一关的模式)，可以手动调整关卡
+   */
+  renderSelectStage(couldSelectStage: boolean) {
+    new SelectStage(couldSelectStage)
+
+    screenView.type = 'selectStage'
   }
 }
